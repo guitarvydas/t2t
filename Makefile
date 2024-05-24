be2be812aa9483304e650f4ed9c0d93ec1ca96f5
Paul Tarvydas <paul.tarvydas@gmail.com>
2024-05-24 03:51:17 -0400

    baseline

---
+++ Makefile
@@ -0,0 +1,5 @@
+all:
+	@echo 'ensure that formatted text option in draw.io is disabled everywhere'
+	./0D/das2json/das2json t2t.drawio
+	./0D/das2json/das2json transpile.drawio
+	python3 main.py . 0D/python in.txt main t2t.drawio.json transpile.drawio.json
---
+++ main.py
@@ -0,0 +1,22 @@
+import py0d as zd
+
+def main ():
+    arg_array = zd.parse_command_line_args ()
+    root_project = arg_array [0] 
+    root_0D = arg_array [1]
+    arg = arg_array [2]
+    main_container_name = arg_array [3]
+    diagram_names = arg_array [4]
+    palette = zd.initialize_component_palette (root_project, root_0D, diagram_names, components_to_include_in_project)
+    zd.run (palette, root_project, root_0D, arg, main_container_name, diagram_names, start_function,
+              show_hierarchy=False, show_connections=False, show_traces=False, show_all_outputs=False)
+
+def start_function (root_project, root_0D, arg, main_container):
+    arg = zd.new_datum_string (arg)
+    msg = zd.make_message("", arg)
+    zd.inject (main_container, msg)
+
+def components_to_include_in_project (root_project, root_0D, reg):
+    pass
+
+main ()
