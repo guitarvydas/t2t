776378c0571536b7578212ea841d49d0ca1bb356
Paul Tarvydas <paul.tarvydas@gmail.com>
2024-05-25 22:39:00 -0400

    parameterized command line

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@
 all:
 	./0D/das2json/das2json t2t.drawio
 	./0D/das2json/das2json transpile.drawio
-	python3 main.py . 0D/python in.txt main t2t.drawio.json transpile.drawio.json
+	python3 main.py . 0D/python swibasm swibasm.ohm swibasm.rwr null.js cleanup cleanup.ohm cleanup.rwr null.js in.txt main t2t.drawio.json transpile.drawio.json
 
 #########
 
--- main.py
+++ main.py
@@ -1,22 +1,117 @@
 import py0d as zd
+import sys
+
+grammar_name = ""
+ohm = ""
+rwr = ""
+support = ""
+
+cleanup_grammar_name = ""
+cleanupohm = ""
+cleanuprwr = ""
+cleanup_support = ""
 
 def main ():
-    arg_array = zd.parse_command_line_args ()
-    root_project = arg_array [0] 
-    root_0D = arg_array [1]
-    arg = arg_array [2]
-    main_container_name = arg_array [3]
-    diagram_names = arg_array [4]
+    global grammar_name, ohm, rwr, support, cleanup_grammar_name, cleanupohm, cleanuprwr, cleanup_support
+
+    print (sys.argv)
+
+    root_project = sys.argv [1] 
+    root_0D = sys.argv [2]
+    i = 3
+    grammar_name = sys.argv [i]
+    i += 1
+    ohm = sys.argv [i]
+    i += 1
+    rwr = sys.argv [i]
+    i += 1
+    support = sys.argv [i]
+    i += 1
+
+    cleanup_grammar_name = sys.argv [i]
+    i += 1
+    cleanupohm = sys.argv [i]
+    i += 1
+    cleanuprwr = sys.argv [i]
+    i += 1
+    cleanup_support = sys.argv [i]
+    i += 1
+
+    arg = sys.argv [i]
+    i += 1
+    main_container_name = sys.argv [i]
+    i += 1
+    diagram_names = sys.argv [i:]
+    i += 1
+
+    print (f'root_project={root_project} root_0D={root_0D}')
+    print (f'grammar_name={grammar_name} ohm={ohm} rwr={rwr} support={support}')
+    print (f'cleanup_grammar_name={cleanup_grammar_name} ohm={cleanupohm} rwr={cleanuprwr} support={cleanup_support}')
+    print (f'arg={arg} main_container_name={main_container_name} diagram_names={diagram_names}')
+ 
     palette = zd.initialize_component_palette (root_project, root_0D, diagram_names, components_to_include_in_project)
     zd.run (palette, root_project, root_0D, arg, main_container_name, diagram_names, start_function,
               show_hierarchy=False, show_connections=False, show_traces=False, show_all_outputs=False)
 
 def start_function (root_project, root_0D, arg, main_container):
+    global grammar_name, ohm, rwr, support, cleanup_grammar_name, cleanupohm, cleanuprwr, cleanup_support
+
+    # grammar name, ohm, rwr, and, support filenames
+    g = zd.new_datum_string (grammar_name)
+    msg = zd.make_message("grammar&#xa;name", g)
+    zd.inject (main_container, msg)
+
+    g = zd.new_datum_string (ohm)
+    msg = zd.make_message("ohm", g)
+    zd.inject (main_container, msg)
+
+    g = zd.new_datum_string (rwr)
+    msg = zd.make_message("rwr", g)
+    zd.inject (main_container, msg)
+
+    g = zd.new_datum_string (support)
+    msg = zd.make_message("support", g)
+    zd.inject (main_container, msg)
+
+    
+
+    g = zd.new_datum_string (cleanup_grammar_name)
+    msg = zd.make_message("cleanup&#xa;grammar&#xa;name", g)
+    zd.inject (main_container, msg)
+
+    g = zd.new_datum_string (cleanupohm)
+    msg = zd.make_message("cleanup&#xa;ohm", g)
+    zd.inject (main_container, msg)
+
+    g = zd.new_datum_string (cleanuprwr)
+    msg = zd.make_message("cleanup&#xa;rwr", g)
+    zd.inject (main_container, msg)
+
+    g = zd.new_datum_string (cleanup_support)
+    msg = zd.make_message("cleanup&#xa;support", g)
+    zd.inject (main_container, msg)
+
+
+    
+    # input source to be t2t'd
     arg = zd.new_datum_string (arg)
     msg = zd.make_message("", arg)
     zd.inject (main_container, msg)
 
 def components_to_include_in_project (root_project, root_0D, reg):
     pass
+
+def t2t_parse_command_line_args ():
+    # return a 5-element array [root_project, root_0D, arg, main_container_name, [diagram_names]]
+    if (len (sys.argv) < (5+1)):
+        load_error ("usage: ${_00_} ${_0D_} app <arg> <main tab name> <diagram file name 1> ...")
+        return None
+    else:
+        root_project = sys.argv [1]
+        root_0D = sys.argv [2]
+        arg = sys.argv [3]
+        main_container_name = sys.argv [4]
+        diagram_source_files = sys.argv [5:]
+        return [root_project, root_0D, arg, main_container_name, diagram_source_files]
 
 main ()
