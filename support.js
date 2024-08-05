36cce17305231ed947d9eff8b687b816915c97dc
Paul Tarvydas <paul.tarvydas@gmail.com>
2024-08-05 10:13:22 -0400

    memo*_predicate ()

--- support.js
+++ support.js
@@ -26,6 +26,7 @@ _ = {
 
     // for rewriter
     parameter_names : [],
+    predicate_expansions : [],
     memo_parameter : function (str) {
 	_.parameter_names.push (str); 
 	return "";
@@ -35,5 +36,9 @@ _ = {
 	_.parameter_names.forEach (p => s.push (`${p}${str}\n`));
 	return s.join ('');
     },
+
+    memo_predicate : function (s) { console.log (`**** MEMO_pred ${s}`); _.predicate_expansions.push (`${s} = ${s}.rwr ();\n`); return ""; },
+    memo_iter_predicate : function (s) { console.log (`**** MEMO_ITER_pred ${s}`); _.predicate_expansions.push (`${s} = ${s}.rwr ().join ('');\n`); return ""; },
+    predicate_expansions_as_string : function () { return _.predicate_expansions.join (''); },
 }
 ,
