121a0cbe695657f09f784ac8e46281bef0db1fc4
Paul Tarvydas <paul.tarvydas@gmail.com>
2024-08-07 20:31:39 -0400

    make auto (simple)

--- simple.mjs
+++ simple.mjs
@@ -21,63 +21,63 @@ example {
 `;
 
 const rewrite_code = {
-Main : function (_pA, _pBsemis, _pBs, _pC, ) {
-let _pre = _.print (`pre down`);
-return_value_stack.push ("");
-rule_name_stack.push ("");
-_.set_top (rule_name_stack, "Main");
-sA_stack.push ('');
-sB_stack.push ('');
-sC_stack.push ('');
+    Main : function (_pA, _pBsemis, _pBs, _pC, ) {
+	let _pre = _.print (`pre down`);
+	return_value_stack.push ("");
+	rule_name_stack.push ("");
+	_.set_top (rule_name_stack, "Main");
+	sA_stack.push ('');
+	sB_stack.push ('');
+	sC_stack.push ('');
 
-_pA = _pA.rwr ();
-_pBsemis = _pBsemis.rwr ().join ('');
-_pBs = _pBs.rwr ().join ('');
-_pC = _pC.rwr ();
+	_pA = _pA.rwr ();
+	_pBsemis = _pBsemis.rwr ().join ('');
+	_pBs = _pBs.rwr ().join ('');
+	_pC = _pC.rwr ();
 
-_.set_top (sA_stack, `${_pA}`);
-_.set_top (sB_stack, `${_pBs}`);
-_.set_top (sC_stack, `${_pC}`);
+	_.set_top (sA_stack, `${_pA}`);
+	_.set_top (sB_stack, `${_pBs}`);
+	_.set_top (sC_stack, `${_pC}`);
 
-_.pre_print (`hello`);
-_.set_top (return_value_stack, `... ${_.print (`middle`)} ${_pA}${_pBsemis}${_.top (sB_stack)}${_pC}...`);
+	_.pre_print (`hello`);
+	_.set_top (return_value_stack, `... ${_.print (`xxx`)} ${_pA}${_pBsemis}${_.top (sB_stack)}${_pC}...`);
 
-_.post_print (`hello`);
-sA_stack.pop ();
-sB_stack.pop ();
-sC_stack.pop ();
+	_.post_print (`hello`);
+	sA_stack.pop ();
+	sB_stack.pop ();
+	sC_stack.pop ();
 
-rule_name_stack.pop ();
-return return_value_stack.pop ();
-},
-Second : function (_pX, _pYsemis, _pYs, _pZ, ) {
-return_value_stack.push ("");
-rule_name_stack.push ("");
-_.set_top (rule_name_stack, "Second");
-sA_stack.push ('');
-sB_stack.push ('');
-sC_stack.push ('');
+	rule_name_stack.pop ();
+	return return_value_stack.pop ();
+    },
+    Second : function (_pX, _pYsemis, _pYs, _pZ, ) {
+	return_value_stack.push ("");
+	rule_name_stack.push ("");
+	_.set_top (rule_name_stack, "Second");
+	sA_stack.push ('');
+	sB_stack.push ('');
+	sC_stack.push ('');
 
-_pX = _pX.rwr ();
-_pYsemis = _pYsemis.rwr ().join ('');
-_pYs = _pYs.rwr ().join ('');
-_pZ = _pZ.rwr ();
+	_pX = _pX.rwr ();
+	_pYsemis = _pYsemis.rwr ().join ('');
+	_pYs = _pYs.rwr ().join ('');
+	_pZ = _pZ.rwr ();
 
-_.set_top (sA_stack, `${_pX}`);
-_.set_top (sB_stack, `${_pYs}`);
-_.set_top (sC_stack, `${_pZ}`);
+	_.set_top (sA_stack, `${_pX}`);
+	_.set_top (sB_stack, `${_pYs}`);
+	_.set_top (sC_stack, `${_pZ}`);
 
-_.pre_print (`hello`);
-_.set_top (return_value_stack, `... ${_.print (`middle`)} ${_pX}${_pYsemis}${_.top (sB_stack)}${_pZ}...`);
+	_.pre_print (`hello`);
+	_.set_top (return_value_stack, `... ${_.print (`middle`)} ${_pX}${_pYsemis}${_.top (sB_stack)}${_pZ}...`);
 
-_.post_print (`hello`);
-sA_stack.pop ();
-sB_stack.pop ();
-sC_stack.pop ();
+	_.post_print (`hello`);
+	sA_stack.pop ();
+	sB_stack.pop ();
+	sC_stack.pop ();
 
-rule_name_stack.pop ();
-return return_value_stack.pop ();
-},
+	rule_name_stack.pop ();
+	return return_value_stack.pop ();
+    },
     _terminal: function () { return this.sourceString; },
     _iter: function (...children) { return children.map(c => c.rwr ()); }
 };
