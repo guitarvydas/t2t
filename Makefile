e6afd7b833706f5841e2c74b96af4cdc59aaf084
Paul Tarvydas <paul.tarvydas@gmail.com>
2024-10-07 05:15:16 -0400

    begin test2 again - failing due to nested `

--- Makefile
+++ Makefile
@@ -16,20 +16,26 @@
 ## make test2t2
 
 dev:
-	node t2t.mjs t2t.grammar t2t.rewrite >dev.t2t.mjs
-	node dev.t2t.mjs test3.rewrite >test3.rwr.mjs
-	cat front.part test3.grammar middle.part test3.rwr.mjs tail.part >dev.test3.mjs
-	node dev.test3.mjs test3.txt
+	@echo '// *** 2 ***'
+	node dev.t2t.mjs test2.rewrite >test2.rwr.mjs
+	cat front.part test2.grammar middle.part test2.rwr.mjs tail.part >dev.test2.mjs
+	node dev.test2.mjs test2.txt
 
 
 devbig:
 	node t2t.mjs t2t.grammar t2t.rewrite >dev.t2t.mjs
 	@echo '// *** 1 ***'
-	@node dev.t2t.mjs test.rewrite
+	node dev.t2t.mjs test.rewrite >test.rwr.mjs
+	cat front.part test.grammar middle.part test.rwr.mjs tail.part >dev.test.mjs
+	node dev.test.mjs test.txt
 	@echo '// *** 2 ***'
-	@node dev.t2t.mjs test2.rewrite
+	node dev.t2t.mjs test2.rewrite >test2.rwr.mjs
+	cat front.part test2.grammar middle.part test2.rwr.mjs tail.part >dev.test2.mjs
+	node dev.test2.mjs test2.txt
 	@echo '// *** 3 ***'
-	@node dev.t2t.mjs test3.rewrite
+	node dev.t2t.mjs test3.rewrite >test3.rwr.mjs
+	cat front.part test3.grammar middle.part test3.rwr.mjs tail.part >dev.test3.mjs
+	node dev.test3.mjs test3.txt
 
 all: self
 
---
+++ dev.test.mjs
@@ -0,0 +1,93 @@
+'use strict'
+
+import {_} from './support.mjs';
+import * as ohm from 'ohm-js';
+
+let _rewrite_support = {
+    verbose : true,
+    top : function (stack) { let v = stack.pop (); stack.push (v); return v; },
+    set_top : function (stack, v) { stack.pop (); stack.push (v); return v; },
+    return_value_stack : [],
+    rule_name_stack : [],
+    depth_prefix : ' ',
+    parameters : {},
+
+    enter_rule : function (name) {
+	if (_rewrite_support.verbose) {
+	    console.error (_rewrite_support.depth_prefix, ["enter", name]);
+	    _rewrite_support.depth_prefix += ' ';
+	}
+	_rewrite_support.return_value_stack.push ("");
+	_rewrite_support.rule_name_stack.push (name);
+    },
+    set_return : function (v) {
+	_rewrite_support.set_top (_rewrite_support.return_value_stack, v);
+    },
+    exit_rule : function (name) {
+	if (_rewrite_support.verbose) {
+	    _rewrite_support.depth_prefix = _rewrite_support.depth_prefix.substr (1);
+	    console.error (_rewrite_support.depth_prefix, ["exit", name]);
+	}
+	_rewrite_support.rule_name_stack.pop ();
+	return _rewrite_support.return_value_stack.pop ()
+    },
+
+    pushParameter : function (name, v) {
+	if (! _rewrite_support.parameters [name]) {
+	    _rewrite_support.parameters [name] = [];
+	}
+	_rewrite_support.parameters [name].push (v);
+    },
+
+    getParameter : function (name) {
+	return _rewrite_support.top (_rewrite_support.parameters [name]);
+    },
+
+    popParameter : function (name) {
+	if (_rewrite_support.parameters [name]) {
+	    _rewrite_support.parameters [name].pop ();
+	}
+    }
+
+};
+
+// for debugging - use when needed
+function dump_stacks () {
+    console.error (_rewrite_support.rule_name_stack, _rewrite_support.return_value_stack, _rewrite_support.parameters);
+}
+
+const grammar = String.raw`
+t2t  {
+  Main = "a"
+}
+`;
+
+let _rewrite = {
+Main : function (c,) {
+_rewrite_support.enter_rule ("Main");
+_rewrite_support.set_return (`hello world ${c.rwr ()}`);
+return _rewrite_support.exit_rule ("Main");
+},
+_terminal: function () { return this.sourceString; },
+_iter: function (...children) { return children.map(c => c.rwr ()); }
+};
+
+// node t2t.mjs test3.txt
+import * as fs from 'fs';
+const argv = process.argv.slice(2);
+console.error (argv);
+let srcFilename = argv[0];
+if ('-' == srcFilename) { srcFilename = 0 }
+let src = fs.readFileSync(srcFilename, 'utf-8');
+console.error (srcFilename);
+console.error (src);
+try {
+    let parser = ohm.grammar (grammar);
+    let cst = parser.match (src);
+    let sem = parser.createSemantics ();
+    sem.addOperation ('rwr', _rewrite);
+    console.log (sem (cst).rwr ());
+}
+catch (e) {
+    console.log (e);
+}
