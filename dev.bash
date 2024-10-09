c3c0f7639a569cbc1a183809bb4a0b442e15cc78
Paul Tarvydas <paul.tarvydas@gmail.com>
2024-10-09 13:50:31 -0400

    snip1

--- dev.bash
+++ dev.bash
@@ -16,23 +16,26 @@
 # # # now, fix up and hand-edit all _.pre_fnxyz (`〔a〕〔b〕`); -->    _.pre_fnxyz (`a`,`b`);
 # # # and import {_} from './bootstrap_support.mjs';
 
+# manually fixed grammar in t2t.mjs
+# now, fix parameter initialization
 
 
-echo
-echo '*** 1 ***'
-node t2t.mjs test.rewrite >test.rwr.mjs
-node test.rwr.mjs
-cat front.part test.grammar middle.part test.rwr.mjs tail.part >new.dev.test.mjs
-node new.dev.test.mjs test.txt
+# echo
+# echo '*** 1 ***'
+# node t2t.mjs test.rewrite >test.rwr.mjs
+# node test.rwr.mjs
+# cat front.part test.grammar middle.part test.rwr.mjs tail.part >new.dev.test.mjs
+# node new.dev.test.mjs test.txt
 
-echo
-echo '*** 2 ***'
-node t2t.mjs test2.rewrite >test2.rwr.mjs
-cat front.part test2.grammar middle.part test2.rwr.mjs tail.part >new.dev.test2.mjs
-node new.dev.test2.mjs test2.txt
+# echo
+# echo '*** 2 ***'
+# node t2t.mjs test2.rewrite >test2.rwr.mjs
+# cat front.part test2.grammar middle.part test2.rwr.mjs tail.part >new.dev.test2.mjs
+# node new.dev.test2.mjs test2.txt
 
 echo
 echo '*** 3 ***'
 node t2t.mjs test3.rewrite >test3.rwr.mjs
 cat front.part test3.grammar middle.part test3.rwr.mjs tail.part >new.dev.test3.mjs
 node new.dev.test3.mjs test3.txt
+
