878a8806275ab396bf937a029ce66b36168aa22b
Paul Tarvydas <paul.tarvydas@gmail.com>
2024-10-10 06:05:32 -0400

    alpha clean compile hand-written t2t.mjs

--- t2t.mjs
+++ t2t.mjs
@@ -1,491 +1,157 @@
+let initilialization = '';
 
-'use strict'
-
-import {_} from './bootstrap_support.mjs';
-import * as ohm from 'ohm-js';
-
-let return_value_stack = [];
-let rule_name_stack = [];
-
-const grammar = String.raw`
-    t2t {
-  main = parameterDef* rewriteDef
-
-  parameterDef = "%" s_ "parameter" s_ name s_
-  rewriteDef = "%" s_ "rewrite" s_ name s_ "{" s_ rewriteRule+ s_ "}" s_
-
-  rewriteRule = ruleName s_ "[" s_ (argDef s_)* "]" s_ "=" s_ rewriteScope s_
-
-  argDef = 
-    | "(" parenarg+ ")" ("+" | "*" | "?")  -- parenthesized
-    | name ("+" | "*" | "?")               -- iter
-    | name                                 -- plain
-
-  rewriteScope =
-    | "⎡" s_ "⎨" s_ name s_ argstring* s_ "⎬" s_ rewriteScope s_ "⎦"      -- call
-    | "⎡" s_  name s_ "=" s_ rewriteFormatString  s_ rewriteScope s_ "⎦"  -- parameterbinding
-    | rewriteFormatString                                                 -- plain
-  
-  rewriteFormatString = "‛" formatItem* "’"
-  formatItem =
-    | "⎨" s_ name s_ argstring* "⎬" -- supportCall
-    | "⟪" parameterRef "⟫"                         -- parameter
-    | "«" argRef "»"                               -- arg
-    | "\\" any                                     -- escapedCharacter
-    | ~"‛" ~"’" ~"⎡" ~"⎦" ~"⟪" ~"⟫" ~"«" ~"»" any  -- rawCharacter
-
-  parenarg = name s_
-  argstring =  rewriteFormatString s_
-  argRef = name
-  parameterRef = name
-  ruleName = name
-
-  name (a name)
-    = nameFirst nameRest*
-  nameFirst = ("_" | letter)
-  nameRest  = ("_" | alnum)
-
-  s_ = space*
-
-}
-`;
-
-const rewrite_js = {
-    main : function (_parameterDefs, _rewriteDef, ) {
-	let parameterDefs = undefined;
-	let rewriteDef = undefined;
-	_.enter_rule ("main");
-	parameterDefs = _parameterDefs.rwr ().join ('')
-	rewriteDef = _rewriteDef.rwr ()
-
-
-	_.set_return (`let _rewrite = {${parameterDefs}${rewriteDef}
+let _rewrite = {
+    main : function (parameterDefs_i, rewriteDef) {
+	return
+`let _rewrite = {
+initializeParameters : function () {
+${parameterDefs_i.rwr ().join ('')}
+},
+${rewriteDef.rwr ()}
 _terminal: function () { return this.sourceString; },
 _iter: function (...children) { return children.map(c => c.rwr ()); }
 };
-`);
-
-	return _.exit_rule ("main");
+`;
     },
-    parameterDef : function (__pct, __1, __parameter, __2, _name, __3, ) {
-	let _pct = undefined;
-	let _1 = undefined;
-	let _parameter = undefined;
-	let _2 = undefined;
-	let name = undefined;
-	let _3 = undefined;
-	_.enter_rule ("parameterDef");
-	_pct = __pct.rwr ()
-	_1 = __1.rwr ()
-	_parameter = __parameter.rwr ()
-	_2 = __2.rwr ()
-	name = _name.rwr ()
-	_3 = __3.rwr ()
 
-
-	_.set_return (``);
-
-	return _.exit_rule ("parameterDef");
+    parameterDef : function (pct, ws1, _parameter, ws2, name, ws3) {
+	return `\n_.freshParameter ("${name.rwr ()}");`
     },
-    rewriteDef : function (__pct, __1, __rewrite, __2, _name, __3, __lb, __4, _rewriteRules, __5, _rb, __6, ) {
-	let _pct = undefined;
-	let _1 = undefined;
-	let _rewrite = undefined;
-	let _2 = undefined;
-	let name = undefined;
-	let _3 = undefined;
-	let _lb = undefined;
-	let _4 = undefined;
-	let rewriteRules = undefined;
-	let _5 = undefined;
-	let rb = undefined;
-	let _6 = undefined;
-	_.enter_rule ("rewriteDef");
-	_pct = __pct.rwr ()
-	_1 = __1.rwr ()
-	_rewrite = __rewrite.rwr ()
-	_2 = __2.rwr ()
-	name = _name.rwr ()
-	_3 = __3.rwr ()
-	_lb = __lb.rwr ()
-	_4 = __4.rwr ()
-	rewriteRules = _rewriteRules.rwr ().join ('')
-	_5 = __5.rwr ()
-	rb = _rb.rwr ()
-	_6 = __6.rwr ()
 
-
-	_.set_return (`${rewriteRules}`);
-
-	return _.exit_rule ("rewriteDef");
+    rewriteDef : function (pct, ws1, _rewrite, ws2, name, ws3, lb, ws4, firstRewriteRule, ws5, subsequentRewriteRule_i, ws6, rb, ws7) {
+	return `${firstRewriteRule.rwr ()}${subsequentRewriteRule_i.rwr ().join ('')}`;
     },
-    rewriteRule : function (_ruleName, __1, _lb, __2, _argDefs, __3s, _rb, __4, __eq, __5, _rewriteScope, __6, ) {
-	let ruleName = undefined;
-	let _1 = undefined;
-	let lb = undefined;
-	let _2 = undefined;
-	let argDefs = undefined;
-	let _3s = undefined;
-	let rb = undefined;
-	let _4 = undefined;
-	let _eq = undefined;
-	let _5 = undefined;
-	let rewriteScope = undefined;
-	let _6 = undefined;
-	_.enter_rule ("rewriteRule");
-	ruleName = _ruleName.rwr ()
-	_1 = __1.rwr ()
-	lb = _lb.rwr ()
-	_2 = __2.rwr ()
-	argDefs = _argDefs.rwr ().join ('')
-	_3s = __3s.rwr ().join ('')
-	rb = _rb.rwr ()
-	_4 = __4.rwr ()
-	_eq = __eq.rwr ()
-	_5 = __5.rwr ()
-	rewriteScope = _rewriteScope.rwr ()
-	_6 = __6.rwr ()
 
-
-	_.set_return (`\n${ruleName} : function (${argDefs}) {
-_rewrite_support.enter_rule ("${ruleName}");${rewriteScope}
-return _rewrite_support.exit_rule ("${ruleName}");
-},`);
-
-	return _.exit_rule ("rewriteRule");
+    firstRewriteRule : function (rewriteRule) {
+	initialization = `\n.initializeParameters ();`;
+	return `${rewriteRule.rwr ()}`;
     },
-    argDef_parenthesized : function (_lp, _names, _rp, _op, ) {
-	let lp = undefined;
-	let names = undefined;
-	let rp = undefined;
-	let op = undefined;
-	_.enter_rule ("argDef_parenthesized");
-	lp = _lp.rwr ()
-	names = _names.rwr ().join ('')
-	rp = _rp.rwr ()
-	op = _op.rwr ()
 
-
-	_.set_return (`${names}`);
-
-	return _.exit_rule ("argDef_parenthesized");
+    subsequentRewriteRule : function (rewriteRule) {
+	initialization = ``;
+	return `${rewriteRule.rwr ()}`;
     },
-    argDef_iter : function (_name, _op, ) {
-	let name = undefined;
-	let op = undefined;
-	_.enter_rule ("argDef_iter");
 
-	name = _name.rwr ()
-	op = _op.rwr ()
-
-
-	_.pre_memoArg (``, `${name}`, `\$\{${name}.rwr ().join ('')\}`);
-
-	_.set_return (`${name},`);
-
-	_.post_memoArg (``, `${name}`, `\$\{${name}.rwr ().join ('')\}`);
-
-	return _.exit_rule ("argDef_iter");
+    // rewriteRule =        ruleName s_    "[" s_   (argDef   s_)*  "]"  s_   "="  s_   rewriteScope  s_
+    rewriteRule : function (ruleName, ws1, lb, ws2, argDef_i, ws3_i, rb, ws4, _eq, ws5, rewriteScope, ws6) {
+	return `\n«ruleName» : function (${argDef_i.rwr ().join ('')}) {
+	    ${initialization}
+	    _rewrite_support.enter_rule ("${ruleName.rwr ()}");${rewriteScope.rwr ()}
+	    return _rewrite_support.exit_rule ("${ruleName.rwr ()}");
+	},`;
     },
-    argDef_plain : function (_name, ) {
-	let name = undefined;
-	_.enter_rule ("argDef_plain");
-
-	name = _name.rwr ()
-
-
-	_.pre_memoArg (``, `${name}`, `\$\{${name}.rwr ()\}`);
-
-	_.set_return (`${name},`);
-
-	_.post_memoArg (``, `${name}`, `\$\{${name}.rwr ()\}〕`);
-
-	return _.exit_rule ("argDef_plain");
+    
+    //argDef = 
+    // | "(" parenarg+ ")" ("+" | "*" | "?")  -- parenthesized
+    argDef_parenthesized : function (lb, parenarg_i, rb, i_op) {
+	return `${parenarg_i.rwr ()}`;
     },
-    rewriteScope_call : function (_lb, __1, _lb2, __a, _fname, __b, _arg, __c, _rb2, __2, _rewriteScope, __3, _rb, ) {
-	let lb = undefined;
-	let _1 = undefined;
-	let lb2 = undefined;
-	let _a = undefined;
-	let fname = undefined;
-	let _b = undefined;
-	let arg = undefined;
-	let _c = undefined;
-	let rb2 = undefined;
-	let _2 = undefined;
-	let rewriteScope = undefined;
-	let _3 = undefined;
-	let rb = undefined;
-	_.enter_rule ("rewriteScope_call");
-	lb = _lb.rwr ()
-	_1 = __1.rwr ()
-	lb2 = _lb2.rwr ()
-	_a = __a.rwr ()
-	fname = _fname.rwr ()
-	_b = __b.rwr ()
-	arg = _arg.rwr ().join ('')
-	_c = __c.rwr ()
-	rb2 = _rb2.rwr ()
-	_2 = __2.rwr ()
-	rewriteScope = _rewriteScope.rwr ()
-	_3 = __3.rwr ()
-	rb = _rb.rwr ()
 
-
-	_.set_return (`\n_.${fname} ("pre", ${arg});\n${rewriteScope}\n_.${fname} ("post", ${arg});`);
-
-	return _.exit_rule ("rewriteScope_call");
+    // | name ("+" | "*" | "?")               -- iter
+    argDef_iter : function (name, i_op) {
+	memoArg (`${name}`, `\$\{${name}.rwr ().join ('')\}`);
+	return `${name},`;
     },
-    rewriteScope_parameterbinding : function (_lb, __1, _pname, __2, __eq, __3, _s, __4, _scope, __5, _rb, ) {
-	let lb = undefined;
-	let _1 = undefined;
-	let pname = undefined;
-	let _2 = undefined;
-	let _eq = undefined;
-	let _3 = undefined;
-	let s = undefined;
-	let _4 = undefined;
-	let scope = undefined;
-	let _5 = undefined;
-	let rb = undefined;
-	_.enter_rule ("rewriteScope_parameterbinding");
-	lb = _lb.rwr ()
-	_1 = __1.rwr ()
-	pname = _pname.rwr ()
-	_2 = __2.rwr ()
-	_eq = __eq.rwr ()
-	_3 = __3.rwr ()
-	s = _s.rwr ()
-	_4 = __4.rwr ()
-	scope = _scope.rwr ()
-	_5 = __5.rwr ()
-	rb = _rb.rwr ()
 
-
-	_.set_return (`\n_rewrite_support.pushParameter ("${pname}", ${s});${scope}\n_rewrite_support.popParameter ("${pname}");`);
-
-	return _.exit_rule ("rewriteScope_parameterbinding");
+    // | name                                 -- plain
+    argDef_plain : function (name) {
+	memoArg (`${name}`, `\$\{${name}.rwr ()\}`);
+	return `${name},`;
     },
-    rewriteScope_plain : function (_s, ) {
-	let s = undefined;
-	_.enter_rule ("rewriteScope_plain");
-	s = _s.rwr ()
 
-
-	_.set_return (`\n_rewrite_support.set_return (${s});`);
-
-	return _.exit_rule ("rewriteScope_plain");
+    // rewriteScope =
+    //   | "⎡" s_ "⎨" s_ name s_ argstring* s_ "⎬" s_ rewriteScope s_ "⎦"      -- call
+    rewriteScope_call : function (lsb, ws1, lb, ws2, fname, ws3, argString_i, ws4, rb, ws5, rewriteScope, ws6, rsb) {
+	return `\n_.${fname.rwr ()} ("pre", ${argString_i.rwr.join ('')}\n${rewriteScope.rwr ()}\n_.${fname.rwr ()} ("post", ${argString_i.rwr.join ('')}`;
     },
-    rewriteFormatString : function (_lq, _formatItems, _rq, ) {
-	let lq = undefined;
-	let formatItems = undefined;
-	let rq = undefined;
-	_.enter_rule ("rewriteFormatString");
-	lq = _lq.rwr ()
-	formatItems = _formatItems.rwr ().join ('')
-	rq = _rq.rwr ()
-
-
-	_.set_return (`\`${formatItems}\``);
-
-	return _.exit_rule ("rewriteFormatString");
+    
+    //   | "⎡" s_  name s_ "=" s_ rewriteFormatString  s_ rewriteScope s_ "⎦"  -- parameterbinding
+    rewriteScope_parameterbinding : function (lsb, ws1, pname, ws2, _eq, ws3, rewriteFormatString, ws4, rewriteScope, ws5, rsb) {
+	return `\npushParameter ("${pname.rwr ()}", ${rewriteFormatString.rwr ()});${rewriteScope.rwr ()}\npopParameter ("${pname}") );`;
     },
-    formatItem_supportCall : function (_lb, __1, _name, __2, _argStrings, _rb, ) {
-	let lb = undefined;
-	let _1 = undefined;
-	let name = undefined;
-	let _2 = undefined;
-	let argStrings = undefined;
-	let rb = undefined;
-	_.enter_rule ("formatItem_supportCall");
-	lb = _lb.rwr ()
-	_1 = __1.rwr ()
-	name = _name.rwr ()
-	_2 = __2.rwr ()
-	argStrings = _argStrings.rwr ().join ('')
-	rb = _rb.rwr ()
-
-
-	_.set_return (`\$\{_.${name} ("", ${argStrings})\}`);
-
-	return _.exit_rule ("formatItem_supportCall");
+    
+    //   | rewriteFormatString                                                 -- plain
+    rewriteScope_plain : function (rewriteFormatString) {
+	return `\nset_return (${rewriteFormatString.rwr ()});`;
     },
-    formatItem_parameter : function (_lb, _parameterRef, _rb, ) {
-	let lb = undefined;
-	let parameterRef = undefined;
-	let rb = undefined;
-	_.enter_rule ("formatItem_parameter");
-	lb = _lb.rwr ()
-	parameterRef = _parameterRef.rwr ()
-	rb = _rb.rwr ()
-
-
-	_.set_return (`${parameterRef}`);
-
-	return _.exit_rule ("formatItem_parameter");
+    
+    // rewriteFormatString = "‛" formatItem* "’"
+    rewriteFormatString : function (lq, formatItem_i, rq) {
+	return `${formatItem_i.rwr ().join ('')}`;
     },
-    formatItem_arg : function (_lb, _argRef, _rb, ) {
-	let lb = undefined;
-	let argRef = undefined;
-	let rb = undefined;
-	_.enter_rule ("formatItem_arg");
-	lb = _lb.rwr ()
-	argRef = _argRef.rwr ()
-	rb = _rb.rwr ()
-
-
-	_.set_return (`${argRef}`);
-
-	return _.exit_rule ("formatItem_arg");
+    
+    // formatItem =
+    //   | "⎨" s_ name s_ argstring* "⎬" -- supportCall
+    formatItem_supportCall : function (lb, ws1, name, ws2, argstring_i, rb) {
+	return `_.\$\{${name.rwr ()} (${argstring_i.rwr ().join ('')})\}`;
     },
-    formatItem_escapedCharacter : function (_bslash, _any, ) {
-	let bslash = undefined;
-	let any = undefined;
-	_.enter_rule ("formatItem_escapedCharacter");
-	bslash = _bslash.rwr ()
-	any = _any.rwr ()
 
-
-	_.set_return (`${bslash}${any}`);
-
-	return _.exit_rule ("formatItem_escapedCharacter");
+    //   | "⟪" parameterRef "⟫"                         -- parameter
+    formatItem_parameter : function (lb, parameterRef, rb) {
+	return `${parameterRef.rwr ()}`;
     },
-    formatItem_rawCharacter : function (_c, ) {
-	let c = undefined;
-	_.enter_rule ("formatItem_rawCharacter");
-	c = _c.rwr ()
+    //   | "«" argRef "»"                               -- arg
 
-
-	_.set_return (`${c}`);
-
-	return _.exit_rule ("formatItem_rawCharacter");
+    formatItem_arg : function (lb, argRef, rb) {
+	return `${argRef.rwr ()}`;
     },
-    parenarg : function (_name, _ws, ) {
-	let name = undefined;
-	let ws = undefined;
-	_.enter_rule ("parenarg");
 
-	name = _name.rwr ()
-	ws = _ws.rwr ()
-
-
-	_.pre_memoArg (``,  `${name}`, `\$\{${name}.rwr ().join ('')\}`);
-
-	_.set_return (`${name},`);
-
-	_.post_memoArg (``, `${name}`, `\$\{${name}.rwr ().join ('')\}`);
-
-	return _.exit_rule ("parenarg");
+    //   | "\\" any                                     -- escapedCharacter
+    formatItem_escapedCharacter : function (backslash, c) {
+	return `${backslash.rwr ()}${c.rwr ()}`;
     },
-    argstring : function (_str, _ws, ) {
-	let str = undefined;
-	let ws = undefined;
-	_.enter_rule ("argstring");
-	str = _str.rwr ()
-	ws = _ws.rwr ()
 
-
-	_.set_return (`${str}, `);
-
-	return _.exit_rule ("argstring");
+    //   | ~"‛" ~"’" ~"⎡" ~"⎦" ~"⟪" ~"⟫" ~"«" ~"»" any  -- rawCharacter
+    formatItem_rawCharacter : function (c) {
+	return `${c.rwr ()}`;
     },
-    argRef : function (_name, ) {
-	let name = undefined;
-	_.enter_rule ("argRef");
-	name = _name.rwr ();
 
-	_.set_return (`${_.fetchArg ('', `${name}`)}`);
-
-	return _.exit_rule ("argRef");
+    // parenarg = name s_
+    parenarg : function (name, ws) {
+	memoArg (name.rwr (), `\$\{${name.rwr ()}.join ('')\}`);
+	return `${name.rwr ()}`;
     },
-    parameterRef : function (_name, ) {
-	let name = undefined;
-	_.enter_rule ("parameterRef");
-	name = _name.rwr ()
 
-
-	_.set_return (`\$\{_rewrite_support.getParameter ("${name}")\}`);
-
-	return _.exit_rule ("parameterRef");
+    // argstring =  rewriteFormatString s_
+    argstring : function (rewriteFormatString, ws) {
+	return `${rewriteFormatString.rwr ()},`;
     },
-    ruleName : function (_name, ) {
-	let name = undefined;
-	_.enter_rule ("ruleName");
-	name = _name.rwr ()
-
-
-	_.set_return (`${name}`);
-
-	return _.exit_rule ("ruleName");
+    
+    // argRef = name
+    argRef : function (name) {
+	return `${fetchArg (name)}`;
     },
-    name : function (_nameFirst, _nameRests, ) {
-	let nameFirst = undefined;
-	let nameRests = undefined;
-	_.enter_rule ("name");
-	nameFirst = _nameFirst.rwr ()
-	nameRests = _nameRests.rwr ().join ('')
-
-
-	_.set_return (`${nameFirst}${nameRests}`);
-
-	return _.exit_rule ("name");
+    
+    // parameterRef = name
+    parameterRef : function (name) {
+	return `\$\{getParameter ("${name.rwr ()}")\}`;
     },
-    nameFirst : function (_c, ) {
-	let c = undefined;
-	_.enter_rule ("nameFirst");
-	c = _c.rwr ()
-
-
-	_.set_return (`${c}`);
-
-	return _.exit_rule ("nameFirst");
+    
+    // ruleName = name
+    ruleName : function (name) {
+	return `${name.rwr ()}`;
     },
-    nameRest : function (_c, ) {
-	let c = undefined;
-	_.enter_rule ("nameRest");
-	c = _c.rwr ()
+    
 
-
-	_.set_return (`${c}`);
-
-	return _.exit_rule ("nameRest");
+    // name (a name)
+    //   = nameFirst nameRest*
+    name : function (nameFirst, nameRest_i) {
+	return `${nameFist.rwr ()}${nameRest_i.rwr ().join ('')}`;
     },
-    s_ : function (_space, ) {
-	let space = undefined;
-	_.enter_rule ("s_");
-	space = _space.rwr ().join ('')
-
-
-	_.set_return (`${space}`);
-
-	return _.exit_rule ("s_");
+    
+    // nameFirst = ("_" | letter)
+    nameFirst : function (c) {
+	return `${c.rwr ()}`;
     },
-    _terminal: function () { return this.sourceString; },
-    _iter: function (...children) { return children.map(c => c.rwr ()); }
+    
+    // nameRest  = ("_" | alnum)
+    nameRest : function (c) {
+	return `${c.rwr ()}`;
+    },
+
+    // s_ = space*
+    s_ : function (ws_i) {
+	return `${ws_i.rwr ().join ('')}`;
+    },
 };
-
-
-
-
-
-// ~~~~~~ main ~~~~~~
-function main (src) {
-    let parser = ohm.grammar (grammar);
-    let cst = parser.match (src);
-    if (cst.succeeded ()) {
-        let cstSemantics = parser.createSemantics ();
-        cstSemantics.addOperation ('rwr', rewrite_js);
-        var generated_code = cstSemantics (cst).rwr ();
-        return generated_code;
-    } else {
-        return cst.message;     
-    }
-}
-
-import * as fs from 'fs';
-const argv = process.argv.slice (2);
-let srcFilename = argv [0];
-let src = fs.readFileSync(srcFilename, 'utf-8');
-var result = main (src);
-console.log (result);
 
