1fc2b481d155808d574b76c951319321a81f57ff
Paul Tarvydas <paul.tarvydas@gmail.com>
2024-10-14 09:32:03 -0400

    add popParameter to end of scope

---
+++ self-compile/temp.ndsl.mjs
@@ -0,0 +1,98 @@
+'use strict'
+
+import * as ohm from 'ohm-js';
+
+let verbose = false;
+
+function top (stack) { let v = stack.pop (); stack.push (v); return v; }
+
+function set_top (stack, v) { stack.pop (); stack.push (v); return v; }
+
+let return_value_stack = [];
+let rule_name_stack = [];
+let depth_prefix = ' ';
+
+function enter_rule (name) {
+    if (verbose) {
+	console.error (depth_prefix, ["enter", name]);
+	depth_prefix += ' ';
+    }
+    return_value_stack.push ("");
+    rule_name_stack.push (name);
+}
+
+function set_return (v) {
+    set_top (return_value_stack, v);
+}
+
+function exit_rule (name) {
+    if (verbose) {
+	depth_prefix = depth_prefix.substr (1);
+	console.error (depth_prefix, ["exit", name]);
+    }
+    rule_name_stack.pop ();
+    return return_value_stack.pop ()
+}
+
+const grammar = String.raw`
+test4  {
+  Main = "a"
+}
+`;
+
+let args = {};
+function resetArgs () {
+    args = {};
+}
+function memoArg (name, accessorString) {
+    args [name] = accessorString;
+};
+function fetchArg (name) {
+    return args [name];
+}
+
+function print (s) {
+    console.log (`PRINT: ${s}`);
+    return "";
+}
+
+function print2 (s1, s2) {
+    console.log (`PRINT2: ${s1} ${s2}`);
+    return "";
+}
+
+let parameters = {};
+function pushParameter (name, v) {
+    parameters [name].push (v);
+}
+function popParameter (name) {
+    parameters [name].pop ();
+}
+function getParameter (name) {
+    return parameters [name];
+}
+
+parameters ["param"] = [];
+
+let _rewrite = {
+
+Main : function (c,) {
+enter_rule ("Main");
+    pushParameter ("param", `${c.rwr ()}`);
+    set_return (`hello world ${print (`the letter is: ${getParameter ("param")} ${c.rwr ()}`,)}`);
+popParameter ("param");
+return exit_rule ("Main");
+},
+_terminal: function () { return this.sourceString; },
+_iter: function (...children) { return children.map(c => c.rwr ()); }
+}
+import * as fs from 'fs';
+const argv = process.argv.slice(2);
+let srcFilename = argv[0];
+if ('-' == srcFilename) { srcFilename = 0 }
+let src = fs.readFileSync(srcFilename, 'utf-8');
+let parser = ohm.grammar (grammar);
+let cst = parser.match (src);
+let sem = parser.createSemantics ();
+sem.addOperation ('rwr', _rewrite);
+console.log (sem (cst).rwr ());
